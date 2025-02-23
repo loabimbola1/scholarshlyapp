0db6fae91cd4eb517e9c759a8c4b5db3970e08c3
loabimbola1 <142395695+loabimbola1@users.noreply.github.com>
2025-02-23 19:53:27 +0100

    Add files via upload

---
+++ v0-user-next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: process.env.NODE_ENV === "production" ? "/your-repo-name" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/your-repo-name" : "",
+}
+
+module.exports = nextConfig
+
